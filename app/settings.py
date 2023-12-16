46a3fe3988d208bb40354a7a8ee4fbf2f28a163e
Saydum <saydum.it5@gmail.com>
2023-12-16 17:54:24 +0300

    create new apps blog and main_page

--- app/settings.py
+++ app/settings.py
@@ -31,7 +31,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'update_server',
+    'main_page',
+    'blog',
 ]
 
 MIDDLEWARE = [
@@ -110,7 +111,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = ['static/']
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+    os.path.join(BASE_DIR, 'main_page/static/main_page'),
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
--- blog/apps.py
+++ blog/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class UpdateServerConfig(AppConfig):
+class BlogConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'update_server'
+    name = 'blog'
